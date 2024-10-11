f66a3d8b32e93495f21008f43b5c4746838e7379
Kishorgujar <91127600+Kishorgujar@users.noreply.github.com>
2024-10-11 13:40:20 +0530

    tags testing-1

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = "mainbucket241011"
   
   tags = {
-    Environment = "AWS"  # Example of a tag for testing
-    Project     = "CICD"
+    Environment = "github"  # Example of a tag for testing
+    Project     = "Pipeline"
   }
 }
