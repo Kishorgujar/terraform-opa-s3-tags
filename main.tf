65afda7a5d68c6dace706c9941abd9135a357c12
Kishorgujar <91127600+Kishorgujar@users.noreply.github.com>
2024-10-11 11:49:31 +0530

    Update both tags for test

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "bucket241011"
   tags = {
-    Environment = "production-updated"  # Example of updated tag for testing
-    Project     = "my_project"
+    Environment = "Testing"  # Example of updated tag for testing
+    Project     = "automation"
   }
 }
