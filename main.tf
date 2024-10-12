a4828aaa598f98468eea9ac3d2540c4717107a55
Kishorgujar <91127600+Kishorgujar@users.noreply.github.com>
2024-10-12 11:11:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,6 +21,6 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = "mainbucket241011"
   
   tags = {
-    Environment = "Test"  # Example of a tag for testing
+    Environment = "DevOps"  # Example of a tag for testing
   }
 }
